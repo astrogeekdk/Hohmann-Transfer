ee00d82cb4cd17e569e74e144e3fbf03e3847e63
astrophy <39146448+astrogeekdk@users.noreply.github.com>
2025-03-23 16:17:49 +0530

    Update hohmann.py

--- hohmann.py
+++ hohmann.py
@@ -1,11 +1,15 @@
 import pygame
+from skyfield.api import load
+import numpy as np
 
 pygame.init()
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 800
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+
 running = True
 
 SUN_MASS = 2e30
@@ -16,56 +20,73 @@ G = 6.67e-11
 
 scale = 1e-9
 
-SUN_EARTH_DIST = 150e9
-SUN_MARS_DIST = 228e9
 
-MARS_ORB_V = 24200
-EARTH_ORB_V = 29800
+planets = load('de421.bsp')
+sun, earth, mars = planets['SUN'], planets['EARTH BARYCENTER'], planets['MARS BARYCENTER']
+ts = load.timescale()
+t = ts.now()
+
+earth_position = earth.at(t).position.m
+earth_velocity = earth.at(t).velocity.m_per_s
+
+mars_position = mars.at(t).position.m
+mars_velocity = mars.at(t).velocity.m_per_s
+
+sun_position = sun.at(t).position.m
+sun_position = pygame.Vector3(sun_position[0], sun_position[1], sun_position[2])
+
+
 
 class Planet:
-    def __init__(self, dist_from_sun, mass, orb_v):
-        self.dist_from_sun = dist_from_sun
+    def __init__(self, mass, v, position):
         self.mass = mass
-        self.orb_v = orb_v
+        self.v = pygame.Vector3(v[0], v[1], v[2])
+        self.position = pygame.Vector3(position[0], position[1], position[2])
 
 class Spacecraft:
-    def __init__(self, dist, mass, initial_v):
-        self.dist = dist
+    def __init__(self, mass, v, position):
         self.mass = mass
-        self.initial_v = initial_v
+        self.v = pygame.Vector3(v[0], v[1], v[2])
+        self.position = pygame.Vector3(position[0], position[1], position[2])
 
 center = pygame.Vector2(SCREEN_WIDTH//2,SCREEN_HEIGHT//2)
 
-SUN_POS = pygame.Vector2(center.x, center.y)
 
-Earth = Planet(SUN_EARTH_DIST, EARTH_MASS, EARTH_ORB_V)
-Mars = Planet(SUN_MARS_DIST, MARS_MASS, MARS_ORB_V)
-        
-Earth.position = pygame.Vector2(center.x+SUN_EARTH_DIST, center.y)
-Earth.v = pygame.Vector2(0, Earth.orb_v)
-
-Mars.position = pygame.Vector2(center.x+SUN_MARS_DIST,center.y)
-Mars.v = pygame.Vector2(0, Mars.orb_v)
+Earth = Planet(EARTH_MASS, earth_velocity, earth_position)
+Mars = Planet(MARS_MASS, mars_velocity, mars_position)
 
 
 planets = [Earth, Mars]
 
+mu = G*SUN_MASS
+r1 = np.linalg.norm(sun_position - earth_position)
+r2 = np.linalg.norm(sun_position - mars_position)
 
-delv1 = (G*SUN_MASS/SUN_EARTH_DIST)**0.5*((2*SUN_MARS_DIST/(SUN_EARTH_DIST+SUN_MARS_DIST))**0.5-1)
+del_v1 = (mu/r1)**0.5 * ((2*r2/(r1+r2))**0.5 - 1)
 
-spacecraft = Spacecraft(0, 10e3, 7e3)
-spacecraft.v = pygame.Vector2(0, EARTH_ORB_V+delv1)
-spacecraft.position = Earth.position + pygame.Vector2(spacecraft.dist, 0)
-print(delv1)
+r_vec = earth_position - sun_position
+v_dir = np.cross(r_vec, [0,0,1])
+v_dir = v_dir / np.linalg.norm(v_dir)
+
+print(v_dir, Earth.v.normalize())
+
+spacecraft = Spacecraft(10e3, Earth.v+v_dir*del_v1, Earth.position)
 
 dt = 100000 
 
+# mode = ["XY", "XZ", "YZ"]
+
+frame = 0
 
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_SPACE:
+        #         spacecraft = Spacecraft(10e3, Earth.v+Earth.v.normalize()*2100, Earth.position)
+
 
 
     # screen.fill("black")
@@ -73,25 +94,27 @@ while running:
     pygame.draw.circle(screen, (255,255,0), center, 10)
 
     for i in range(len(planets)):
-        force = G*planets[i].mass*SUN_MASS/(SUN_POS-planets[i].position).length_squared()
-        force_direction = (SUN_POS-planets[i].position).normalize()
+        force = G*planets[i].mass*SUN_MASS/(sun_position-planets[i].position).length_squared()
+        force_direction = (sun_position-planets[i].position).normalize()
         a = force/planets[i].mass*force_direction
         planets[i].v += a*dt
         planets[i].position += planets[i].v* dt
 
-    force = (G*spacecraft.mass*Earth.mass/(Earth.position-spacecraft.position).length()**2)*(Earth.position-spacecraft.position).normalize()
-    force += (G*spacecraft.mass*SUN_MASS/(SUN_POS-spacecraft.position).length()**2)*(SUN_POS-spacecraft.position).normalize()
-    force += (G*spacecraft.mass*Mars.mass/(Mars.position-spacecraft.position).length()**2)*(Mars.position-spacecraft.position).normalize()
-    a = force/spacecraft.mass
-    spacecraft.v += a*dt
-    spacecraft.position += spacecraft.v * dt
+    if spacecraft:
+        force = (G*spacecraft.mass*Earth.mass/(Earth.position-spacecraft.position).length()**2)*(Earth.position-spacecraft.position).normalize()
+        force += (G*spacecraft.mass*SUN_MASS/(sun_position-spacecraft.position).length()**2)*(sun_position-spacecraft.position).normalize()
+        force += (G*spacecraft.mass*Mars.mass/(Mars.position-spacecraft.position).length()**2)*(Mars.position-spacecraft.position).normalize()
+        a = force/spacecraft.mass
+        spacecraft.v += a*dt
+        spacecraft.position += spacecraft.v * dt
+        pygame.draw.circle(screen, (255,255,255), (center.x+spacecraft.position.x*scale, center.y+spacecraft.position.y*scale), 1)
 
 
 
         
+    pygame.draw.circle(screen, (255,255,0), (center.x, center.y), 5)
     pygame.draw.circle(screen, (0,255,0), (center.x+Earth.position.x*scale, center.y+Earth.position.y*scale), 2)
     pygame.draw.circle(screen, (255,0,0), (center.x+Mars.position.x*scale, center.y+Mars.position.y*scale), 2)
-    pygame.draw.circle(screen, (255,255,255), (center.x+spacecraft.position.x*scale, center.y+spacecraft.position.y*scale), 2)
 
 
     pygame.display.flip()
